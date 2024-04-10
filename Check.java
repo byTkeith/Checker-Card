a09dc604b7fa39cfb9aa57dadd456f0553750ad8
Your Name <you@example.com>
2024-04-10 23:30:59 +0200

    added the scanner method

--- Check.java
+++ Check.java
@@ -7,6 +7,7 @@ import java.io.*;
 import java.util.*;
 public class Check {
     public static void main(String[] args) {
-        
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter the name of the cards file:");
     }
 }
