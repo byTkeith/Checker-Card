e05fb8ae6577148bd2eb32a0eb503086a42af73c
Your Name <you@example.com>
2024-04-10 23:40:16 +0200

    added an array list to add cards

--- Check.java
+++ Check.java
@@ -7,7 +7,23 @@ import java.io.*;
 import java.util.*;
 public class Check {
     public static void main(String[] args) {
+        ArrayList<String> cards=new ArrayList<>();//to add the card lines from the file
         Scanner scanner =new Scanner(System.in);
         System.out.println("Enter the name of the cards file:");
+        String filename= scanner.nextLine();
+        try{File file= new File(filename);
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                cards.add(scanner.nextLine());
+            }
+
+        }catch(FileNotFoundException e){
+            System.out.println("File not Found");
+        }finally{
+             if(scanner!=null){
+                scanner.close();
+             }
+             
+        }
     }
 }
